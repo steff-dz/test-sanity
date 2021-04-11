ff45c593c7e80df035cdc5d5a8d6bf06f4a8d61c
Stefanie Dziadkowiec <stefaniemarie.dz@gmail.com>
2021-04-11 16:47:58 +0200

    finished up test portfolio from sanity codealong

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,19 @@
-import React from 'react'
+import React from "react";
+import image from "../randombackground.jpg";
 
-export default function Home(){
-    return <h1>Home Page</h1>
+export default function Home() {
+  return (
+    <main>
+      <img
+        src={image}
+        alt="desk with potted plant background"
+        className="absolute object-cover w-full h-full"
+      />
+      <section className="relative flex justify-center min-h-screen pt-12 lg:pt-20 px-8">
+        <h1 className="text-6xl text-black font-bold cursive leading-none lg:leading-snug home-name">
+          Howdy. I'm Steff
+        </h1>
+      </section>
+    </main>
+  );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,32 +1,66 @@
-import React from 'react'
-import {NavLink} from 'react-router-dom'
-import {SocialIcon} from 'react-social-icons'
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { SocialIcon } from "react-social-icons";
 
-
-export default function NavBar(){
-    return (
-        <header className="bg-yellow-300">
-            <div className="container mx-auto flex justify-between">
-                <nav className="flex">
-                    <NavLink to="/" exact activeClassName="text-white" className="inflex-flex items-center py-6 px-3 mr-4 text-white-100 hover:text-green-800 text-4xl font-bold cursive tracking-widest" >
-                        Steff
-                    </NavLink>
-                    <NavLink to="/post" activeClassName="text-white bg-yellow-400" className="inline-flex items-center py-3 px-3 my-6 rounded hover:text-green-800">
-                        Blog Post
-                    </NavLink>
-                    <NavLink to="/project" activeClassName="text-white bg-yellow-400" className="inline-flex items-center py-3 px-3 my-6 rounded hover:text-green-800">
-                        Projects
-                    </NavLink>
-                    <NavLink to="/about" activeClassName="text-white bg-yellow-400" className="inline-flex items-center py-3 px-3 my-6 rounded hover:text-green-800">
-                        About Me 
-                    </NavLink>
-                </nav>
-                <div className="inline-flex py-3 px-3 my-6">
-                    <SocialIcon url="https://www.linkedin.com/in/stefanie-dz/" className="mr-4" target="_blank" fgColor="#fff" style={{height: 35, width: 35}}/>
-                    <SocialIcon url="https://github.com/" className="mr-4" target="_blank" fgColor="#fff" style={{height: 35, width: 35}}/>
-                    <SocialIcon url="https://www.instagram.com/stefanie_dziadkowiec/" className="mr-4" target="_blank" fgColor="#fff" style={{height: 35, width: 35}}/>
-                </div>
-            </div>
-        </header>
-    )
+export default function NavBar() {
+  return (
+    <header className="bg-black">
+      <div className="container mx-auto flex justify-between">
+        <nav className="flex">
+          <NavLink
+            to="/"
+            exact
+            activeClassName="text-white"
+            className="inflex-flex items-center py-6 px-3 mr-4 text-white-100 hover:text-green-800 text-4xl font-bold cursive tracking-widest"
+          >
+            Steff
+          </NavLink>
+          <NavLink
+            to="/post"
+            activeClassName="text-white bg-yellow-400"
+            className="inline-flex items-center py-3 px-3 my-6 rounded text-white hover:text-green-800"
+          >
+            Blog Post
+          </NavLink>
+          <NavLink
+            to="/project"
+            activeClassName="text-white bg-yellow-400"
+            className="inline-flex items-center py-3 px-3 my-6 text-white rounded hover:text-green-800"
+          >
+            Projects
+          </NavLink>
+          <NavLink
+            to="/about"
+            activeClassName="text-white bg-yellow-400"
+            className="inline-flex items-center py-3 px-3 my-6 text-white rounded hover:text-green-800"
+          >
+            About Me
+          </NavLink>
+        </nav>
+        <div className="inline-flex py-3 px-3 my-6">
+          <SocialIcon
+            url="https://www.linkedin.com/in/stefanie-dz/"
+            className="mr-4"
+            target="_blank"
+            fgColor="#fff"
+            style={{ height: 35, width: 35 }}
+          />
+          <SocialIcon
+            url="https://github.com/"
+            className="mr-4"
+            target="_blank"
+            fgColor="#fff"
+            style={{ height: 35, width: 35 }}
+          />
+          <SocialIcon
+            url="https://www.instagram.com/stefanie_dziadkowiec/"
+            className="mr-4"
+            target="_blank"
+            fgColor="#fff"
+            style={{ height: 35, width: 35 }}
+          />
+        </div>
+      </div>
+    </header>
+  );
 }
